95c90e2d45ffc5f53c0306e40693a287c06b32dc
suji <borra.sujani@gmail.com>
2023-04-26 15:28:36 -0400

    security groups

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -1,6 +1,12 @@
-data "aws_security_group" "selected" {
-  name = "allow-all"
-}
-output "security_group" {
-  value = data.aws_security_group.selected.id
+#data "aws_security_group" "selected" {
+#  name = "allow-all"
+#}
+#output "security_group" {
+#  value = data.aws_security_group.selected.id
+#}
+
+data "aws_security_groups" "test" {}
+
+output "all_sg" {
+ value = data.aws_security_group.test
 }
