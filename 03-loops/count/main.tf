7565d7e4468e94dfc91b52d7e04bbc8157b89cbc
suji <borra.sujani@gmail.com>
2023-04-26 16:02:10 -0400

    loops

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,3 +1,15 @@
 resource "null_resourse" "null"{
   count = 10
 }
+
+resource "null_resourse" "fruits"{
+  count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo Fruit name- ${var.fruits[count.index]}"
+  }
+}
+
+variable "fruits"{
+  default = ["apple", "banana", "orange"]
+}
