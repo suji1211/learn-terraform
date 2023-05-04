20b2246131c0102c5ac3fbbb7049dbc1e18e5e60
suji <borra.sujani@gmail.com>
2023-05-04 11:32:53 -0400

    functions

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -10,8 +10,19 @@ variable "sample1" {
   default = ["abc", "xyz"]
 }
 
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+  }
+}
+
 ## print first value like var.sample1[0] with a function
 
 output "sample1" {
-  value = element(var.sample, 0)
+  value = element(var.sample1, 0)
+}
+
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
 }
