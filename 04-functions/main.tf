70c532a77ca89838c05632e423c5c0ed2c902c85
suji <borra.sujani@gmail.com>
2023-05-04 11:29:03 -0400

    functions

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -5,3 +5,13 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+
+variable "sample1" {
+  default = ["abc", "xyz"]
+}
+
+## print first value like var.sample1[0] with a function
+
+output "sample1" {
+  value = element(var.sample, 0)
+}
